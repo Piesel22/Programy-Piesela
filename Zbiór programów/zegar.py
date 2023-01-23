5ad4121c0d1480fe25baac08abd8c9f661bce926
Piesel22 <igorpopowicz12@gmail.com>
2023-01-23 11:44:40 +0100

    zegar update

--- Zbiór programów/zegar.py
+++ Zbiór programów/zegar.py
@@ -3,13 +3,9 @@ import PySimpleGUI as sg
 from datetime import datetime
 
 def run():
-    now = datetime.now()
-    window = sg.Window("Zegar", [[sg.Text(now)]])
     while True:
+        window = sg.Window("Zegar", [[sg.Text(datetime.now())]])
         event, values = window.read()
         if event == sg.WIN_CLOSED:
             break
-        now = datetime.now()
-        window.update("Zegar", [[sg.Text(now)]])
-        window.refresh()
     window.close()
