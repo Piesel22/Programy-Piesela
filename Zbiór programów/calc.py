1f5b147221dd4230c73e782cc0fd952518b4dade
Piesel22 <igorpopowicz12@gmail.com>
2023-01-24 11:46:13 +0100

    Naprawiono problemy z ułamkami

--- Zbiór programów/calc.py
+++ Zbiór programów/calc.py
@@ -1,5 +1,6 @@
 import PySimpleGUI as sg
 import math
+from decimal import Decimal
 
 def run():
     layout = [[sg.Text("Podaj pierwszą liczbę"), sg.InputText()],[sg.Text("Podaj drugą liczbę"), sg.InputText()], [sg.Button("+"), sg.Button("-"), sg.Button("*"), sg.Button("/"), sg.Button("^")], [sg.Button("Zaokrąglenie"), sg.Button("sin"), sg.Button("cos"), sg.Button("Exp")]]
@@ -10,39 +11,39 @@ def run():
       if event == sg.WIN_CLOSED:
         break
       if event == "+":
-        layout2 = [[sg.Text("Wynik działania:")], [sg.Text(float(values[0]) + float(values[1]))]]
+        layout2 = [[sg.Text("Wynik działania:")], [sg.Text(Decimal(values[0]) + Decimal(values[1]))]]
         window2 = sg.Window("Wynik", layout2)
         window2.read()
       if event == "-":
-        layout2 = [[sg.Text("Wynik działania:")], [sg.Text(float(values[0]) - float(values[1]))]]
+        layout2 = [[sg.Text("Wynik działania:")], [sg.Text(Decimal(values[0]) - Decimal(values[1]))]]
         window2 = sg.Window("Wynik", layout2)
         window2.read()
       if event == "*":
-        layout2 = [[sg.Text("Wynik działania:")], [sg.Text(float(values[0]) * float(values[1]))]]
+        layout2 = [[sg.Text("Wynik działania:")], [sg.Text(Decimal(values[0]) * Decimal(values[1]))]]
         window2 = sg.Window("Wynik", layout2)
         window2.read()
       if event == "/":
-        layout2 = [[sg.Text("Wynik działania:")], [sg.Text(float(values[0]) / float(values[1]))]]
+        layout2 = [[sg.Text("Wynik działania:")], [sg.Text(Decimal(values[0]) / Decimal(values[1]))]]
         window2 = sg.Window("Wynik", layout2)
         window2.read()
       if event == "^":
-        layout2 = [[sg.Text("Wynik działania:")], [sg.Text(float(values[0])**float(values[1]))]]
+        layout2 = [[sg.Text("Wynik działania:")], [sg.Text(Decimal(values[0])**Decimal(values[1]))]]
         window2 = sg.Window("Wynik", layout2)
         window2.read()
       if event == "Zaokrąglenie":
-        layout2 = [[sg.Text("Wynik działania:")], [sg.Text(round(float(values[0])))]]
+        layout2 = [[sg.Text("Wynik działania:")], [sg.Text(round(Decimal(values[0])))]]
         window2 = sg.Window("Wynik", layout2)
         window2.read()
       if event == "sin":
-        layout2 = [[sg.Text("Wynik działania:")], [sg.Text(float(math.sin(float(values[0]))))]]
+        layout2 = [[sg.Text("Wynik działania:")], [sg.Text(math.sin(Decimal(values[0])))]]
         window2 = sg.Window("Wynik", layout2)
         window2.read()
       if event == "cos":
-        layout2 = [[sg.Text("Wynik działania:")], [sg.Text(float(math.sin(float(values[0]))))]]
+        layout2 = [[sg.Text("Wynik działania:")], [sg.Text(math.cos(Decimal(values[0])))]]
         window2 = sg.Window("Wynik", layout2)
         window2.read()
       if event == "Exp":
-        layout2 = [[sg.Text("Wynik działania:")], [sg.Text(float(math.exp(float(values[0]))))]]
+        layout2 = [[sg.Text("Wynik działania:")], [sg.Text((math.exp(Decimal(values[0]))))]]
         window2 = sg.Window("Wynik", layout2)
         window2.read()
     window.close()
